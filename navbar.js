2c698009e02769cf17e5d3bd66e875290945a42f
Arek21 <56130666+Arek21@users.noreply.github.com>
2019-11-22 09:02:34 +0100

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,13 @@
+let nav =  '<h1>Nawigacja</h1>'+
+                                  '<ul>'+
+                                      '<li>'+
+                                        '<a href="index.html">  index </a>'+
+                                      '</li>'+
+                                      '<li>'+
+                                        '<a href="kontakt.html"> kontakt </a>'+
+                                      '</li>'+
+                                    '</ul>';
+
+
+                      let element = document.querySelector('.nav-bar');
+                      element.innerHTML=nav;
---
+++ script.js
@@ -0,0 +1,4 @@
+function zmiana(){
+   var pole = document.getElementById("akcja");
+   akcja.style.backgroundColor = "red";
+   }
